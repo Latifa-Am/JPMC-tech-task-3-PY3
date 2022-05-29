d717800730b56163300128b818a3d7ba80c47926
Latifa-Am <latifaamahri20@gmail.com>
2022-05-29 14:42:18 +0100

    Create Patch File

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -21,38 +21,49 @@ class Graph extends Component<IProps, {}> {
   componentDidMount() {
     // Get element from the DOM.
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
-
+    //Ading the ration field to track ratios of two stocks, also lower/upper bounds
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
+      price_abc: 'float',
+      price_def: 'float',
+      ratio: 'float',
       timestamp: 'date',
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'float',
     };
 
     if (window.perspective && window.perspective.worker()) {
       this.table = window.perspective.worker().table(schema);
     }
     if (this.table) {
-      // Load the `table` in the `<perspective-viewer>` DOM reference.
-      elem.load(this.table);
+      /*
+       Load the `table` in the `<perspective-viewer>` DOM reference.
+      ‘view’ is the the kind of graph we wanted to visualize the data as (set to y_line). 
+      ‘row-pivots’ takes care of our x-axis. This allows us to map each datapoint based on the timestamp it has.
+      ‘columns’ is what will allow us to only focus on a particular part of a datapoint’s data along the y-axis.
+      ‘aggregates’ is what will allow us to handle the cases of duplicated data.
+      */
+      elem.load(this.table); 
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('columns', '["ration","lower_bound","upper_bound","trigger_alert"]');
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
         timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
       }));
     }
   }
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
+      this.table.update([
         DataManipulator.generateRow(this.props.data),
-      );
+      ]);
     }
   }
 }
